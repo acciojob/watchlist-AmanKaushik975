438500e58205f1275c66b9d47a3f61755cba4453
amank <aman.kaushik.975@gmail.com>
2024-06-25 18:41:55 +0530

    first'

--- src/main/java/com/driver/MovieRepository.java
+++ src/main/java/com/driver/MovieRepository.java
@@ -1,6 +1,9 @@
 package com.driver;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
@@ -19,28 +22,47 @@ public class MovieRepository {
 
     public void saveMovie(Movie movie){
         // your code here
+        movieMap.put(movie.getName(),movie);
     }
 
     public void saveDirector(Director director){
         // your code here
+        directorMap.put(director.getName(),director);
     }
 
     public void saveMovieDirectorPair(String movie, String director){
         if(movieMap.containsKey(movie) && directorMap.containsKey(director)){
-            // your code here
+            movieMap.put(movie, movieMap.get(movie));
+            directorMap.put(director, directorMap.get(director));
+            List<String> currentMovies = new ArrayList<>();
+            if(directorMovieMapping.containsKey(director)) currentMovies = directorMovieMapping.get(director);
+            currentMovies.add(movie);
+            directorMovieMapping.put(director, currentMovies);
         }
     }
 
     public Movie findMovie(String movie){
         // your code here
+        if(movieMap.containsKey(movie)){
+            return movieMap.get(movie);
+        }
+        return new Movie();
     }
 
     public Director findDirector(String director){
         // your code here
+        if(directorMap.containsKey(director)){
+            return directorMap.get(director);
+        }
+        return new Director();
     }
 
     public List<String> findMoviesFromDirector(String director){
         // your code here
+        if(directorMovieMapping.containsKey(director)){
+            return directorMovieMapping.get(director);
+        }
+        return new ArrayList<>();
     }
 
     public List<String> findAllMovies(){
@@ -49,9 +71,39 @@ public class MovieRepository {
 
     public void deleteDirector(String director){
         // your code here
+        //movies = new ArrayList<String>();
+        if(directorMovieMapping.containsKey(director)){
+            List<String> movies = directorMovieMapping.get(director);
+            for(String movie: movies){
+                if(movieMap.containsKey(movie)){
+                    movieMap.remove(movie);
+                }
+            }
+
+            directorMovieMapping.remove(director);
+        }
+
+        if(directorMap.containsKey(director)){
+            directorMap.remove(director);
+        }
     }
 
     public void deleteAllDirector(){
         // your code here
+        HashSet<String> moviesSet = new HashSet<>();
+
+        //directorMap = new HashMap<>();
+
+        for(String director: directorMovieMapping.keySet()){
+            for(String movie: directorMovieMapping.get(director)){
+                moviesSet.add(movie);
+            }
+        }
+
+        for(String movie: moviesSet){
+            if(movieMap.containsKey(movie)){
+                movieMap.remove(movie);
+            }
+        }
     }
 }
